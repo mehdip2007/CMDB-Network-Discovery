63d857eed2b681c3fc349284a7ee0f3e11b7b0c6
Mehdi <mehdipourhadi@hotmail.com>
2021-11-10 17:48:53 +0330

    add huawei oss and ericsson devices

---
+++ main_ftp_ericsson.py
@@ -0,0 +1,15 @@
+from modules import ftp_ericsson
+from modules import extract_and_csv
+import os
+
+hostname = "enm1.mtnirancell.ir"
+user = "factsusr"
+passwd = "ffff4444"
+port = 5022
+
+ftp = ftp_ericsson.Ssh(host=hostname, username=user, password=passwd, port=port, timeout=10)
+ftp.download_files()
+
+cwd = os.getcwd()
+extract_and_csv.unzip(cwd)
+extract_and_csv.xml_to_csv(cwd)
