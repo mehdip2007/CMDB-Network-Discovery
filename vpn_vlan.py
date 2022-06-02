c0a47ffb675b0d31ce475c40711c518a69ce3720
Mehdi <mehdipourhadi@hotmail.com>
2022-06-02 15:58:23 +0430

    add VLAN files

---
+++ vpn_vlan.py
@@ -0,0 +1,16 @@
+vpn_vlan_id = {
+
+    "VLAN_ID": "3002",
+    "VLAN_ID": "3004",
+    "VLAN_ID": "3005",
+    "VLAN_ID": "3006",
+    "VLAN_ID": "3007",
+    "VLAN_ID": "3010",
+    "VLAN_ID": "3011",
+    "VLAN_ID": "3012",
+    "VLAN_ID": "3013",
+    "VLAN_ID": "3014",
+    "VLAN_ID": "3015",
+    "VLAN_ID": "3016",
+
+}
